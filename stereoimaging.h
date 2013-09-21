40bfe60758a4bfe489d2004ecc26c7aa61d1a4b3
RUPPESH NALWAYA <ruppeshnalwaya1993@gmail.com>
2013-09-22 03:07:49 +0530

    Update stereoimaging.h

--- stereoimaging.h
+++ stereoimaging.h
@@ -31,26 +31,38 @@ namespace StereoImag
         Size size;
     } Rectangle;
 
+
+    /* there are two images LEFT and RIGHT
+       Both images have set of corrensponding pixels
+       Candidate Pixel is a pixel point in LEFT image that is tested
+       for correspondence to a specific pixel point in the RIGHT image
+    */
     class CandidatePixel
     {
     public:
-        PixPoint point;
-        long int totalColorDiff;
-        long int noPixelsMatchedWith;
-        double distanceFromOriginalCoord;
-        double score;
+        PixPoint point;    		 // x and y coordinates
+        long int totalColorDiff;         // aggregate of diffence in the pixel value of surrounding pixels
+        long int noPixelsMatchedWith;    // number of surrounding pixels considered to calculate differences and score
+        double distanceFromOriginalCoord;   //unused
+        double score;              // tells the level of similarity of candidate pixel to a specific pixel
 
     public :
         CandidatePixel();
         CandidatePixel(PixPoint p);
         CandidatePixel(PixPoint p, long int t, long int n, double d, double s);
         CandidatePixel(CandidatePixel &cp);
-        void computeScore();
+        void computeScore();             
         static bool compare(CandidatePixel* cp1, CandidatePixel* cp2);
     };
 
     typedef vector<CandidatePixel*> CPlist;
 
+
+    /*
+    class that implements methods to find
+    pixels corresponding to a specific pixel in other image and 
+    rank them by similarity
+    */
     class StereoImaging
     {
     public:
@@ -68,11 +80,12 @@ namespace StereoImag
         static PixPoint correspondPointv2(Image& img1, Image& img2, PixPoint inPoint, Rectangle rect, StereoImag::Size dp);
     };
 
+
     class Constants
     {
     public:
-        static const MatchingLevel MAX_MATCHING_LEVEL;
-        static const double THRESHHOLD_SCORE;
+        static const MatchingLevel MAX_MATCHING_LEVEL;  //size of rectangle around the specific pixel point coordinate to be scanned in other image
+        static const double THRESHHOLD_SCORE;   //thresholded/;imiting maximum difference between pixel values for qualifyPointsAbsolute()
     };
 
 
